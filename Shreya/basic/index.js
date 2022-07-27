09f3229cc9e1b70ee92338e8857c2b6d0cd6f7de
Shreya20-web <shreyabhardwaj102@gmail.com>
2022-07-27 13:09:10 +0530

    basic react project

---
+++ Shreya/basic/index.js
@@ -0,0 +1,16 @@
+function App() {
+
+    return(
+        <h1>Heloo JSX</h1>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"))
+
+
+// app.use(function(req, res, next){
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
